3d350403b453e482585f824ed1c5587390ac07d5
Lea Kalmar <74097846+momoirox@users.noreply.github.com>
2023-01-24 17:57:09 +0100

    test 2

--- fakultet/pmf/views.py
+++ fakultet/pmf/views.py
@@ -40,7 +40,7 @@ def index(request):
                     st.save()
                     response = "Sudent registered ."
                 elif(response.status_code == 409):
-                    response = "Student allre  ady exists."
+                    response = "Student allready exists."
                 else : 
                     response = "Internal server error. Sorry, try again :( ."
 
